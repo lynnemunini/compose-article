2304a725bf0876b9c616d535f17b3008239e1d78
lynnemunini <muninilynne65@gmail.com>
2022-10-17 15:54:47 +0300

    Position textS AND images on screen

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.data.EmptyGroup.name
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Article()
                 }
             }
         }
@@ -32,7 +39,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Article() {
-    Text(text = "Hello $name!")
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            "Jetpack Compose tutorial",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
+
+        )
+        Text(
+            "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.\n",
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
